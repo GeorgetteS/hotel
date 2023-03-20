2939e59b9e41522b5cac1932ba87f7a7877397d2
GeorgetteS <103926946+GeorgetteS@users.noreply.github.com>
2023-03-20 16:59:03 +0300

    Spinner

--- react-frontend/src/pages/Room.js
+++ react-frontend/src/pages/Room.js
@@ -2,106 +2,45 @@ import { useParams } from 'react-router-dom';
 
 import { useEffect, useState } from 'react';
 
-// import RoomInfo from '../components/Bookingpage/RoomPage/RoomInfo';
-// import RoomList from '../components/Bookingpage/RoomPage/RoomList';
-
 import RoomList from '../components/RoomPage/RoomList';
 
 import RoomInfo from '../components/RoomPage/RoomInfo';
 
-// [
-// 	{
-// 		"title": "Стандартный",
-// 		"id": "standart",
-// 		"text": "Насладитесь отдыхом в уютном люксе площадью 70 кв. м. В номере есть просторная гостиная, удобное рабочее место, душевая с ванной и большая кровать (King).",
-// 		"images": [
-// 			"../img/rooms/room2.jpg",
-// 			"https://www.travelline.ru/resource/images/rt/194026/637910628843190800-ce3f23bb-d26f-4e0d-b083-30e26dc1b932",
-// 			"https://www.travelline.ru/resource/images/rt/194026/637910628822527120-31e9c9ca-84f5-4301-8fe2-763bf4c1efd4"
-// 		],
-// 		"icons": [
-// 			"./img/snowflake.svg",
-// 			"./img/tv.svg",
-// 			"./img/hairdryer.svg"
-// 		],
-// 		"countOfQuests": 4,
-// 		"premium": false
-
-// 	},
-// 	{
-// 		"title": "Полулюкс-Студия",
-// 		"id": "junior",
-// 		"text": "Насладитесь роскошью делюкса площадью 67 кв.м. с большой гостиной, панорамными окнами. В номере есть удобное рабочее место, большая кровать (King) и просторная ванная комната с душевой кабиной и отдельно стоящей ванной.",
-// 		"images": [
-// 			"../img/rooms/room5.png",
-// 			"https://www.travelline.ru/resource/images/rt/194071/637910678694312426-5a4d711c-2b93-42b4-bb96-2b1eb65ad6ef",
-// 			"https://zvezda-slavhotels.ru/upload/resize_cache/iblock/346/985_545_0/r4eijgplx960onqreg2f7vqu6ftwk0ai.jpeg"
-// 		],
-// 		"icons": [
-// 			"./img/tv.svg",
-// 			"./img/hairdryer.svg",
-// 			"./img/snowflake.svg"
-// 		],
-// 		"countOfQuests": 5,
-// 		"premium": false
-
-// 	},
-// 	{
-// 		"title": "ЛЮКС",
-// 		"id": "suite",
-// 		"text": "Насладитесь роскошью делюкса площадью 67 кв.м. с большой гостиной, панорамными окнами. В номере есть удобное рабочее место, большая кровать (King) и просторная ванная комната с душевой кабиной и отдельно стоящей ванной.",
-// 		"images": [
-// 			"../img/rooms/room7.png",
-// 			"https://zvezda-slavhotels.ru/upload/resize_cache/iblock/051/985_545_0/gjxz697s1ebeqx2u24qqo39bhk48ux6t.jpeg",
-// 			"https://zvezda-slavhotels.ru/upload/resize_cache/iblock/c49/985_545_0/nflbha1pg09gh3gsd72kb71d65zo7p0c.jpeg"
-// 		],
-// 		"icons": [
-// 			"./img/tv.svg",
-// 			"./img/hairdryer.svg",
-// 			"./img/snowflake.svg"
-// 		],
-// 		"countOfQuests": 5,
-// 		"premium": true
-
-// 	},
-// 	{
-// 		"title": "Бизнес",
-// 		"id": "business",
-// 		"text": "Насладитесь отдыхом в уютном люксе площадью 70 кв. м. В номере есть просторная гостиная, удобное рабочее место, душевая с ванной и большая кровать (King).",
-// 		"images": [
-// 			"../img/rooms/room6.jpg",
-// 			"https://zvezda-slavhotels.ru/upload/resize_cache/iblock/8cb/985_545_0/7n1ock17hso068m3r6ofdxaji3k4klk5.jpeg",
-// 			"https://www.travelline.ru/resource/images/rt/194071/637910678694312426-5a4d711c-2b93-42b4-bb96-2b1eb65ad6ef"
-// 		],
-// 		"icons": [
-// 			"./img/snowflake.svg",
-// 			"./img/tv.svg",
-// 			"./img/hairdryer.svg"
-// 		],
-// 		"countOfQuests": 4,
-// 		"premium": true
-
-// 	}
-// ]
+import Spinner from '../components/Spinner';
 
 function Room() {
   const [room, setRoom] = useState();
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   const { type } = useParams();
 
-  useEffect(() => {
-    function fetchRooms() {
-      fetch(`https://640ed1cc4ed25579dc3d72f1.mockapi.io/rooms/${type}`)
+  async function fetchRooms() {
+    try {
+      setLoading(true);
+      await fetch(`https://640ed1cc4ed25579dc3d72f1.mockapi.io/rooms/${type}`)
         .then((res) => res.json())
         .then((data) => {
           setRoom(data);
         });
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
     }
+  }
+
+  useEffect(() => {
     fetchRooms();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [type]);
 
   if (!room) {
     return;
+  }
+
+  if (error) {
+    return <main>Беда!</main>;
   }
 
   return (
@@ -109,8 +48,7 @@ function Room() {
       <div className="container">
         <div className="room__body">
           <RoomList id={room} />
-
-          <RoomInfo {...room} />
+          {isLoading ? <Spinner /> : <RoomInfo {...room} />}
         </div>
       </div>
     </main>
--- react-frontend/src/pages/Rooms.js
+++ react-frontend/src/pages/Rooms.js
@@ -2,47 +2,47 @@ import { useState, useEffect } from 'react';
 
 import Card from '../components/Card';
 import RoomsMain from '../components/RoomsPage/RoomsMain';
+import Spinner from '../components/Spinner';
 
 function Rooms() {
   const [rooms, setRooms] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   const fetchRooms = async () => {
-    await fetch('https://640ed1cc4ed25579dc3d72f1.mockapi.io/rooms')
-      .then((res) => res.json())
-      .then((data) => {
-        setRooms(data);
-      });
-  };
-
-  useEffect(() => {
     try {
-      fetchRooms();
+      await fetch('https://640ed1cc4ed25579dc3d72f1.mockapi.io/rooms')
+        .then((res) => res.json())
+        .then((data) => {
+          setRooms(data);
+        });
     } catch (error) {
+      setError(true);
     } finally {
       setLoading(false);
     }
+  };
+
+  useEffect(() => {
+    fetchRooms();
   }, []);
+
+  if (error) {
+    return <main>Беда!</main>;
+  }
+
+  const roomCards = rooms.map((item) => <Card key={item.id} {...item} type={item.id}></Card>);
 
   return (
     <main className="rooms">
       <RoomsMain />
-
-      {isLoading ? (
-        <div>'Загрузка...'</div>
-      ) : (
-        <div className="rooms__gallery gallery">
-          <div className="container">
-            <div className="gallery__item">
-              <div className="gallery__row">
-                {rooms.map((item, index) => {
-                  return <Card key={item.type} {...item} type={item.id}></Card>;
-                })}
-              </div>
-            </div>
+      <div className="rooms__gallery gallery">
+        <div className="container">
+          <div className="gallery__item">
+            <div className="gallery__row">{isLoading ? <Spinner /> : roomCards}</div>
           </div>
         </div>
-      )}
+      </div>
     </main>
   );
 }
